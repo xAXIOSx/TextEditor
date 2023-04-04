7c952edbc8211aaec536dbde2b1bbe9a7c74350f
Axios <diman0719192323@gmail.com>
2023-04-04 22:35:42 +0500

    test2

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,9 +10,10 @@ import { defineComponent } from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
+  data(){
+    return {
+      msg:'3213213231'
+    }
+  }
 });
 </script>
